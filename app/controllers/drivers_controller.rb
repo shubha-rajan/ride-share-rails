50ebf64c9aaf3277f5871a682317cc2291ccb2fb
Shubha Rajan <shubharajan94@gmail.com>
2019-04-15 16:20:08 -0700

    added index and show methods and views for driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,7 +3,12 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def show
+    @driver = Driver.find_by(id: params[:id])
+  end
+
   private
+
   def driver_params
     return params.require(:driver).permit(:name, :vin)
   end
