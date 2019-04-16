8580d712fa945340b8331bd5476b8fa559119382
Shubha Rajan <shubharajan94@gmail.com>
2019-04-16 15:32:16 -0700

    edit/update methods and views for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,10 +5,35 @@ class PassengersController < ApplicationController
 
   def show
     @passenger = Passenger.find_by(id: params[:id])
+    unless @passenger
+      head :not_found
+    end
   end
 
-private
-def passenger_params
-  return params.require(:passenger).permit(:name, :phone_num)
-end
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+    unless @passenger
+      head :not_found
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if passenger
+      if @passenger.update passenger_params
+        redirect_to passenger_path(passenger_id)
+      else
+        render new
+      end
+    else
+      head :not_found
+    end
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
 end
