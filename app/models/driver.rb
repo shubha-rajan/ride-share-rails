2e5d043ffe8b14f1cbedb8841a09130b37bca323
Shubha Rajan <shubharajan94@gmail.com>
2019-04-17 19:30:48 -0700

    wrote method to calculate total earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,10 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings
+    trips = self.trips
+    total_earnings = 0.8 * (trips.reduce(0) { |total, trip| total += trip.cost })
+    total_earnings -= 165 if ((trips.length > 0) && (total_earnings > 165))
+    return total_earnings
+  end
 end
