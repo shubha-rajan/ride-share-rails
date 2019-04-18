4e0387e1ccab60bde9f8d25c6fa660b94f50d2a9
Shubha Rajan <shubharajan94@gmail.com>
2019-04-18 14:52:53 -0700

    added avg rating, total earnings, and delete link to driver show view. Added avg rating method to driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,12 +2,18 @@ class Driver < ApplicationRecord
   has_many :trips, dependent: :nullify
 
   validates :name, presence: true
-  validates :vin, presence: true, length: {is: 17}
+  validates :vin, presence: true, length: { is: 17 }
 
   def total_earnings
     trips = self.trips
     total_earnings = 0.8 * (trips.reduce(0) { |total, trip| total += trip.cost })
     total_earnings -= 165 if ((trips.length > 0) && (total_earnings > 165))
     return total_earnings
+  end
+
+  def average_rating
+    ratings = self.trips.map { |trip| trip.rating }
+    average_rating = (ratings.sum.to_f / ratings.length).round(2)
+    retrn average_rating
   end
 end
